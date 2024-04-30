4c6100dc95df2d9d990260ebe23d0d3dcd62eee9
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-30 06:38:57 -0700

    Init

---
+++ pages/src/routes/layout.tsx
@@ -0,0 +1,40 @@
+import { component$, Slot } from '@builder.io/qwik';
+import type { DocumentHead, RequestHandler } from '@builder.io/qwik-city';
+
+export const head: DocumentHead = {
+	title: 'TODO',
+	meta: [
+		{
+			name: 'description',
+			content: 'TODO',
+		},
+	],
+};
+
+/**
+ * @link https://qwik.dev/docs/middleware/#locale
+ */
+export const onRequest: RequestHandler = async ({ locale, request }) => {
+	const acceptLanguage = request.headers.get('accept-language');
+	const [languages] = acceptLanguage?.split(';') || ['?', '?'];
+	const [preferredLanguage] = languages!.split(',');
+	locale(preferredLanguage);
+};
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+	// Control caching for this request for best performance and to reduce hosting costs:
+	// https://qwik.dev/docs/caching/
+	cacheControl({
+		// Always serve a cached response by default, up to a week stale
+		staleWhileRevalidate: 60 * 60 * 24 * 7,
+		// Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+		maxAge: 5,
+	});
+};
+
+export default component$(() => {
+	// Setup contexts
+
+	// UI
+	return <Slot />;
+});
---
+++ pages/vite.config.ts
@@ -0,0 +1,61 @@
+import { qwikCity } from '@builder.io/qwik-city/vite';
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { defineConfig, type UserConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import type { EnvVars } from './src/types';
+
+// https://developers.cloudflare.com/workers/runtime-apis/nodejs
+const cloudflareNodeRuntimes: `node:${string}`[] = ['node:assert', 'node:async_hooks', 'node:buffer', 'node:crypto', 'node:diagnostics_channel', 'node:events', 'node:path', 'node:process', 'node:stream', 'node:string_decoder', 'node:test', 'node:util'];
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({ command, mode }): UserConfig => {
+	return {
+		plugins: [
+			qwikCity(),
+			qwikVite(),
+			tsconfigPaths(),
+			nodeResolve({
+				browser: true,
+				modulesOnly: true,
+				preferBuiltins: true,
+			}),
+		],
+		server: {
+			headers: {
+				// Don't cache the server response in dev mode
+				'Cache-Control': 'public, max-age=0',
+			},
+		},
+		preview: {
+			headers: {
+				// Do cache the server response in preview (non-adapter production build)
+				'Cache-Control': 'public, max-age=600',
+			},
+		},
+		build: {
+			target: 'esnext',
+			sourcemap: (process.env as EnvVars).NODE_ENV !== 'production',
+			emptyOutDir: true,
+			rollupOptions: {
+				external: [...cloudflareNodeRuntimes],
+			},
+			manifest: true,
+		},
+		worker: {
+			rollupOptions: {
+				external: [...cloudflareNodeRuntimes],
+			},
+		},
+		ssr: {
+			external: [...cloudflareNodeRuntimes],
+		},
+		optimizeDeps: {
+			/**
+			 * Put problematic deps that break bundling here, mostly those with binaries.
+			 * For example ['better-sqlite3'] if you use that in server functions.
+			 */
+			exclude: [...cloudflareNodeRuntimes],
+		},
+	};
+});
