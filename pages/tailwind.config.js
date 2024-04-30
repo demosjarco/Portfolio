17fa60af3dd33ba67623dd840a75cc9e97b02ac0
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-30 13:18:27 -0700

    Add custom animations

--- pages/tailwind.config.js
+++ pages/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
 			screens: {
 				xxs: '320px',
 			},
+			keyframes: {
+				powerOffAnimation: {
+					'0%': { filter: 'brightness(1) grayscale(0)' },
+					'30%': { filter: 'brightness(1) grayscale(0)' },
+					'100%': { filter: 'brightness(0.6) grayscale(1)' },
+				},
+			},
 		},
 		fontFamily: {
 			sans: ['Tahoma', 'Noto Sans', 'ui-sans-serif'],
