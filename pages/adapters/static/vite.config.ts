efe3057772f59cf0cc404167cd13af578061bb4d
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-30 08:37:10 -0700

    Change to static

---
+++ pages/adapters/static/vite.config.ts
@@ -0,0 +1,20 @@
+import { staticAdapter } from '@builder.io/qwik-city/adapters/static/vite';
+import { extendConfig } from '@builder.io/qwik-city/vite';
+import baseConfig from '../../vite.config';
+
+export default extendConfig(baseConfig, () => {
+	return {
+		build: {
+			rollupOptions: {
+				input: ['@qwik-city-plan'],
+			},
+			ssrManifest: true,
+			ssr: true,
+		},
+		plugins: [
+			staticAdapter({
+				origin: 'https://yoursite.qwik.dev',
+			}),
+		],
+	};
+});
