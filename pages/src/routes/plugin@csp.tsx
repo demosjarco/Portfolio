61ce6d1f484d726903c6498db1f1b7a91d6ffb9c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-30 09:15:19 -0700

    Actually run on static

--- pages/src/routes/plugin@csp.tsx
+++ pages/src/routes/plugin@csp.tsx
@@ -256,7 +256,7 @@ class CSPGenerator {
 }
 
 export const onRequest: RequestHandler = ({ platform, sharedMap, headers }) => {
-	if (isDev || ('static' in platform && platform.static) || runningLocally(platform.request)) return; // Will not return CSP headers in dev mode
+	if (isDev || ((!('static' in platform) || ('static' in platform && !platform.static)) && runningLocally(platform.request))) return; // Will not return CSP headers in dev mode
 
 	const csp = new CSPGenerator();
 	sharedMap.set('@nonce', csp.nonce);
