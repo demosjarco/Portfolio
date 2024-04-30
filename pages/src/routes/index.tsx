a237fe86ab7f9610ac65f5a86ceccf2b687399e6
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-30 13:18:59 -0700

    Root container

--- pages/src/routes/index.tsx
+++ pages/src/routes/index.tsx
@@ -1,17 +1,9 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import WinXp from '../WinXp';
 
 export default component$(() => {
-	return (
-		<>
-			<h1>Hi 👋</h1>
-			<p>
-				Can't wait to see what you build with qwik!
-				<br />
-				Happy coding.
-			</p>
-		</>
-	);
+	return <WinXp />;
 });
 
 export const head: DocumentHead = ({ url }) => {
