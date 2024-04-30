b037129cbd1a08d6200df65d49d869c0d6234953
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-30 13:17:29 -0700

    Move defaults

---
+++ pages/src/contexts/signals.tsx
@@ -0,0 +1,5 @@
+import { Focusing, PowerState, type IconStates } from './types';
+
+export const defaultFocus: Focusing = Focusing.window;
+export const defaultIconState: IconStates = [];
+export const defaultState: PowerState = PowerState.start;
