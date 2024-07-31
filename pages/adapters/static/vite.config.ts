a9458b06783684de5a195c6d0685c69ccad2902a
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-31 13:49:55 -0700

    Fix vite.config

--- pages/adapters/static/vite.config.ts
+++ pages/adapters/static/vite.config.ts
@@ -1,8 +1,9 @@
 import { staticAdapter } from '@builder.io/qwik-city/adapters/static/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
+import type { UserConfig } from 'vite';
 import baseConfig from '../../vite.config';
 
-export default extendConfig(baseConfig, () => {
+export default extendConfig(baseConfig, (): UserConfig => {
 	return {
 		build: {
 			rollupOptions: {
--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -7,25 +7,21 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import type { EnvVars } from './src/types';
 
 // https://developers.cloudflare.com/workers/runtime-apis/nodejs
-const cloudflareNodeRuntimes: `node:${string}`[] = ['node:assert', 'node:async_hooks', 'node:buffer', 'node:crypto', 'node:diagnostics_channel', 'node:events', 'node:path', 'node:process', 'node:stream', 'node:string_decoder', 'node:test', 'node:util'];
+const cloudflareNodeRuntimes: `node:${string}`[] = ['node:assert', 'node:async_hooks', 'node:buffer', 'node:crypto', 'node:diagnostics_channel', 'node:events', 'node:path', 'node:process', 'node:stream', 'node:string_decoder', 'node:util'];
+const cloudflareRuntimes: `cloudflare:${string}`[] = ['cloudflare:email'];
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default defineConfig(({ command, mode }): UserConfig => {
+export default defineConfig((): UserConfig => {
 	return {
 		plugins: [
 			qwikCity(),
-			qwikVite({
-				devTools: {
-					clickToSource: false,
-				},
-			}),
+			qwikVite(),
 			tsconfigPaths(),
-			vanillaExtractPlugin(),
 			nodeResolve({
 				browser: true,
 				modulesOnly: true,
 				preferBuiltins: true,
 			}),
+			vanillaExtractPlugin(),
 		],
 		server: {
 			headers: {
@@ -44,24 +40,23 @@ export default defineConfig(({ command, mode }): UserConfig => {
 			sourcemap: (process.env as EnvVars).NODE_ENV !== 'production',
 			emptyOutDir: true,
 			rollupOptions: {
-				external: [...cloudflareNodeRuntimes],
+				external: [...cloudflareNodeRuntimes, ...cloudflareRuntimes],
 			},
 			manifest: true,
 		},
 		worker: {
 			rollupOptions: {
-				external: [...cloudflareNodeRuntimes],
+				external: [...cloudflareNodeRuntimes, ...cloudflareRuntimes],
 			},
 		},
 		ssr: {
-			external: [...cloudflareNodeRuntimes],
+			external: [...cloudflareNodeRuntimes, ...cloudflareRuntimes],
 		},
+		// This tells Vite which dependencies to pre-build in dev mode.
 		optimizeDeps: {
-			/**
-			 * Put problematic deps that break bundling here, mostly those with binaries.
-			 * For example ['better-sqlite3'] if you use that in server functions.
-			 */
-			exclude: [...cloudflareNodeRuntimes],
+			// Put problematic deps that break bundling here, mostly those with binaries.
+			// For example ['better-sqlite3'] if you use that in server functions.
+			exclude: [...cloudflareNodeRuntimes, ...cloudflareRuntimes],
 		},
 	};
 });
