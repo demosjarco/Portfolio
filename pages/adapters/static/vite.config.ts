87ce36ecedbdaf18abc922d4c1c38e2f4c98705c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-31 14:28:24 -0700

    ts wrong

--- pages/adapters/static/vite.config.ts
+++ pages/adapters/static/vite.config.ts
@@ -2,6 +2,7 @@ import { staticAdapter } from '@builder.io/qwik-city/adapters/static/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
 
+// @ts-ignore
 export default extendConfig(baseConfig, () => {
 	return {
 		build: {
