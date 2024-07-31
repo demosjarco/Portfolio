36e8f6bb22e1ca22e8e163ad56d7014c5777c67c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-31 14:47:46 -0700

    Fix eslint

--- pages/.eslintrc.cjs
+++ pages/.eslintrc.cjs
@@ -1,5 +1,8 @@
+/**
+ * @type {import('eslint').Linter.BaseConfig}
+ * @link https://github.com/cloudflare/workers-sdk/blob/main/packages/eslint-config-worker/index.js
+ */
 module.exports = {
-	root: true,
 	env: {
 		browser: true,
 		serviceworker: true,
@@ -7,8 +10,7 @@ module.exports = {
 		// https://eslint.org/docs/head/use/configure/language-options-deprecated#specifying-environments
 		es2024: true,
 	},
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:qwik/recommended'],
-	parser: '@typescript-eslint/parser',
+	extends: ['../.eslintrc.cjs', 'plugin:qwik/recommended'],
 	parserOptions: {
 		tsconfigRootDir: __dirname,
 		project: ['./tsconfig.json'],
@@ -18,28 +20,10 @@ module.exports = {
 			jsx: true,
 		},
 	},
-	plugins: ['@typescript-eslint'],
+	plugins: ['qwik'],
 	rules: {
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-inferrable-types': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/no-empty-interface': 'off',
-		'@typescript-eslint/no-namespace': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/no-this-alias': 'off',
-		'@typescript-eslint/ban-types': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'prefer-spread': 'off',
-		'no-case-declarations': 'off',
-		'no-console': 'off',
 		// Note: you must disable the base rule as it can report incorrect errors
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': 'error',
-		'@typescript-eslint/no-unnecessary-condition': 'warn',
 		'qwik/jsx-img': 'off',
-		'@typescript-eslint/no-import-type-side-effects': 'error',
-		'@typescript-eslint/consistent-type-imports': 'error',
 		// We use visible task to defer `server$()` from page load
 		'qwik/no-use-visible-task': 'off',
 	},
