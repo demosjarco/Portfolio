836a4c78714a1b0022c7f586977363c44ff42475
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-31 13:49:55 -0700

    Fix vite.config

--- pages/adapters/cloudflare-pages/vite.config.ts
+++ pages/adapters/cloudflare-pages/vite.config.ts
@@ -1,8 +1,9 @@
 import { cloudflarePagesAdapter } from '@builder.io/qwik-city/adapters/cloudflare-pages/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
+import type { UserConfig } from 'vite';
 import baseConfig from '../../vite.config';
 
-export default extendConfig(baseConfig, () => {
+export default extendConfig(baseConfig, (): UserConfig => {
 	return {
 		build: {
 			rollupOptions: {
