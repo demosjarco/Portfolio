952794529edca699151d0d544bdb5d0ef39160a0
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-09-15 22:14:35 -0700

    Not needed

--- pages/src/WinXp/Taskbar/start/index.tsx
+++ pages/src/WinXp/Taskbar/start/index.tsx
@@ -4,14 +4,14 @@ import XpLogo from '~/assets/xp.svg?jsx';
 export default component$(() => {
 	return (
 		<div
-			class="flex flex-col items-center rounded-r-lg pl-2 pr-3"
+			class="flex flex-col rounded-r-lg pl-2 pr-3"
 			style={{
 				background: 'linear-gradient(180deg, #5ca05c 3%, #8cbe8b 6%, #75b275 10%, #64ab64 12%, #4e9f4d 15%, #499e48 18%, #469e46 20%, #45a045 23%, #43a543 38%, #48aa41 86%, #47b747 89%, #46b246 92%, #44ad44 95%, #3c963c 98%)',
 				'border-right': '1px solid #1042af',
 				'box-shadow': 'inset -1px 0 1px #206320, inset 1px 1px 2px rgba(255, 255, 255, 0.5)',
 				'text-shadow': '1px 1px 1px rgba(0,0,0,0.5)',
 			}}>
-			<span class="">
+			<span>
 				<XpLogo
 					class="inline-block h-5 align-bottom"
 					style={{
