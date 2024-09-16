7d47f2a773e00c6da91a14209c4b3ec3cc76d0d0
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-09-15 22:40:28 -0700

    easter egg

--- pages/src/WinXp/Taskbar/taskbarWindows/index.tsx
+++ pages/src/WinXp/Taskbar/taskbarWindows/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
-	return <div class="grow overflow-x-scroll">Taskbar Windows</div>;
+	return <div class="grow overflow-x-scroll">CSS is hard</div>;
 });
