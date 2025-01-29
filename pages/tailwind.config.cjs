8affb1aa94e866af8666c62853d174c95db63f6c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2025-01-29 01:42:35 -0700

    Carry over `flowbite`

--- pages/tailwind.config.cjs
+++ pages/tailwind.config.cjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,jsx,tsx,mdx,css}', './node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {
 			screens: {
@@ -19,5 +18,4 @@ module.exports = {
 			mono: ['Lucida Console', 'ui-monospace'],
 		},
 	},
-	plugins: [require('flowbite/plugin')],
 };
