1ebe1f56a75075fa5ffa495bf52f750ed3da8074
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2026-06-15 17:27:24 -0700

    Add GraphQL codegen, DB schema & CI step
    Introduce GraphQL Codegen config and generated client files, plus CI integration to run codegen and commit generated artifacts. Add new GraphQL sources under src/gql, scheduled tasks, and a sql cache utility. Update MSE DB migration and schema to add description and ja3 columns and include a DebugLogWriter for Drizzle logging. Tweak dependabot to include @graphql-codegen/* packages and adjust CI/workflow/envs to support code generation and Cloudflare deployment.

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,5 @@ export interface EnvVars extends Omit<Cloudflare.Env, ''>, TypedBindings {
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface TypedBindings {}
+
+export const DB_MSE_D1_ID = '59912d5a-9a3f-472d-89a7-9309f7f45f4f' as const;
